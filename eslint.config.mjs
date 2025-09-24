fe60d5539b90132e4a05d781a9553a5e1c1ef807
Helwiza Fahry <helwyza@gmail.com>
2025-09-24 21:42:29 +0700

    x

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -23,6 +23,7 @@ const eslintConfig = [
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unused-vars": "off",
       "@typescript-eslint/ban-ts-comment": "off",
+      "react/no-unescaped-entities": "off",
     },
   },
 ];
